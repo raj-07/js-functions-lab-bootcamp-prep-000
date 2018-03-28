5d9887aadf566fcfa6fa996614b0922bd22e8013
Raj CHOWDHURY <rajchowdhury.rc@gmail.com>
2018-03-28 00:06:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 function happyHolidays(){
   return "Happy Holidays!"
 }
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo (name, holiday){
+  return ` Happy ${holiday},${name}!`
+}
+
+function holidayCountdown (holiday,days){
+  return `It's ${days} days until ${holiday}!`
+}
